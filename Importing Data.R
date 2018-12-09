24fc431a648236eb498c7e77eed3ff5dfc1e34ec
Willamar <wfs20@hotmail.com>
2018-12-09 21:44:24 -0200

    Atualizado com novos comando aprendidos no curso

--- Importing Data.R
+++ Importing Data.R
@@ -7,7 +7,7 @@ str(pools)
 pools <- read.csv("DataSets/swimming_pools.csv", stringsAsFactors = FALSE)
 str(pools)
 
-# Para ler arquivos txts usamos a função read.delim()
+# Podemos usar a função read.delim() para ler arquivos
 # Por padrão ele irá dividir os campos pelo delimitador \t (tab)
 # e a primeira linha ele defini como o cabeçalho
 hotdogs <- read.delim("DataSets/hotdogs.txt")
@@ -80,3 +80,177 @@ potatoes_fragment <- read_tsv("DataSets/potatoes.txt", skip = 6, n_max = 5, col_
 # c - character, d double, i integer, l logical, _ skips
 potatoes_char <- read_tsv("DataSets/potatoes.txt", col_types = "cccccccc", col_names = properties)
 str(potatoes_char)
+
+# Outra maneira de difinir os tipos das colunas é usando o collectors
+# as funções collector devem ser passadas para uma lista e como argumento de col_types
+fac <- col_factor(levels = c("Beef", "Meat", "Poultry"))
+int <- col_integer()
+hotdogs_factor <- read_tsv("DataSets/hotdogs.txt",
+                           col_names = c("type", "calories", "sodium"),
+                           col_types = list(fac, int, int))
+summary(hotdogs_factor)
+
+
+#fread é um pacote facil e rapido de usar, é muito parecido com read.table()
+# muitas vezes só precisamos especificar o caminho do arquivo para que os nossos dados possam ser importados com sucesso
+install.packages("data.table")
+library(data.table)
+
+potatoes <- fread("DataSets/potatoes.csv")
+potatoes
+
+
+#fread - drop e select
+#drop - seleciona as colunas que não quero importar do arquivo
+#select - seleciona as colunas que quero importar do arquivo
+
+potatoes <- fread("DataSets/potatoes.csv", select = c(6,8))
+potatoes <- fread("DataSets/potatoes.csv", drop = c(1:5,7))
+potatoes <- fread("DataSets/potatoes.csv", select = c('texture','moistness'))
+potatoes <- fread("DataSets/potatoes.csv", drop = c('area','temp','size','storage','method','flavor'))
+plot(potatoes$texture, potatoes$moistness)
+
+potatoes_fread <- fread("DataSets/potatoes.csv")
+potatoes_readr <- read_csv("DataSets/potatoes.csv")
+
+class(potatoes_fread)
+class(potatoes_readr)
+
+
+# Lendo arquivos xlsx com a pacote readxl
+install.packages("readxl")
+library(readxl)
+
+
+excel_sheets("DataSets/urbanpop.xlsx")
+
+pop_1 <- read_excel("DataSets/urbanpop.xlsx", sheet = 1)
+pop_2 <- read_excel("DataSets/urbanpop.xlsx", sheet = 2)
+pop_3 <- read_excel("DataSets/urbanpop.xlsx", sheet = "1975-2011")
+pop_list <- list(pop_1, pop_2, pop_3)
+str(pop_list)
+
+pop_list <- lapply(excel_sheets("DataSets/urbanpop.xlsx"), read_excel, path="DataSets/urbanpop.xlsx")
+str(pop_list)
+
+# Por default o read_excel irá importar o arquivo com o header
+pop_a <- read_excel("DataSets/urbanpop.xlsx", sheet = 1, col_names = FALSE)
+cols <- c("country", paste0("year_", 1960:1966))
+pop_b <- read_excel("DataSets/urbanpop.xlsx", sheet = 1, col_names = cols)
+
+summary(pop_a)
+summary(pop_b)
+
+# Argumento skip irá pular as linhas
+urbanpop_skip <- read_excel("DataSets/urbanpop.xlsx", sheet = 2, skip = 21, col_names = FALSE)
+urbanpop <- read_excel("DataSets/urbanpop.xlsx", sheet = 2, col_names = FALSE)
+
+dim(urbanpop_skip)
+dim(urbanpop)
+
+
+install.packages("gdata")
+library(gdata)
+
+urban_pop <- read.xls("DataSets/urbanpop.xls", sheet="1967-1974")
+head(urban_pop, 11)
+
+columns <- c("country", paste0("year_", 1967:1974))
+
+urban_pop <- read.xls("DataSets/urbanpop.xls", sheet = 2,
+                      skip = 50, header = FALSE, stringsAsFactors = FALSE,
+                      col.names = columns)
+head(urban_pop, 10)
+
+path <- "C:/Projetos/Importing-Cleaning-Data-in-R/DataSets/urbanpop.xls"
+urban_sheet1 <- read.xls(path, sheet = 1, stringsAsFactors = FALSE)
+urban_sheet2 <- read.xls(path, sheet = 2, stringsAsFactors = FALSE)
+urban_sheet3 <- read.xls(path, sheet = 3, stringsAsFactors = FALSE)
+urban <- cbind(urban_sheet1, urban_sheet2[-1], urban_sheet3[-1])
+# na.omit irá retirar todas as linhas com valores NA
+urban_clean <- na.omit(urban)
+summary(urban)
+summary(urban_clean)
+
+
+
+
+install.packages("XLConnect")
+library(XLConnect)
+my_book <- loadWorkbook("DataSets/urbanpop.xlsx")
+class(my_book)
+
+my_book <- loadWorkbook("DataSets/urbanpop.xlsx")
+getSheets(my_book)
+readWorksheet(my_book, sheet = 2)
+
+# Build connection to urbanpop.xlsx
+my_book <- loadWorkbook("DataSets/urbanpop.xlsx")
+
+# Import columns 3, 4, and 5 from second sheet in my_book: urbanpop_sel
+urbanpop_sel <- readWorksheet(my_book, sheet = 2, startCol = 3, endCol = 5)
+
+# Import first column from second sheet in my_book: countries
+countries <- readWorksheet(my_book, sheet = 2, startCol = 1, endCol = 1)
+
+# cbind() urbanpop_sel and countries together: selection
+selection <- cbind( countries, urbanpop_sel)
+
+
+# Build connection to urbanpop.xlsx
+my_book <- loadWorkbook("urbanpop.xlsx")
+
+# Add a worksheet to my_book, named "data_summary"
+createSheet(my_book,"data_summary")
+
+# Use getSheets() on my_book
+getSheets(my_book)
+
+
+
+# XLConnect is already available
+
+# Build connection to urbanpop.xlsx
+my_book <- loadWorkbook("urbanpop.xlsx")
+
+# Add a worksheet to my_book, named "data_summary"
+createSheet(my_book, "data_summary")
+
+# Create data frame: summ
+sheets <- getSheets(my_book)[1:3]
+dims <- sapply(sheets, function(x) dim(readWorksheet(my_book, sheet = x)), USE.NAMES = FALSE)
+summ <- data.frame(sheets = sheets,
+                   nrows = dims[1, ],
+                   ncols = dims[2, ])
+
+# Add data in summ to "data_summary" sheet
+writeWorksheet(my_book, summ, "data_summary")
+
+# Save workbook as summary.xlsx
+saveWorkbook(my_book,"summary.xlsx")
+
+
+# my_book is available
+
+# Rename "data_summary" sheet to "summary"
+renameSheet(my_book, "data_summary", "summary")
+
+# Print out sheets of my_book
+getSheets(my_book)
+
+# Save workbook to "renamed.xlsx"
+saveWorkbook(my_book, "renamed.xlsx")
+
+
+# Load the XLConnect package
+library(XLConnect)
+
+# Build connection to renamed.xlsx: my_book
+my_book <- loadWorkbook("renamed.xlsx")
+
+# Remove the fourth sheet
+removeSheet(my_book, sheet = 4)
+
+# Save workbook to "clean.xlsx"
+saveWorkbook(my_book, "clean.xlsx")
+
