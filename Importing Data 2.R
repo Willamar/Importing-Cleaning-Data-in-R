53212da61e762c1f0f770ee021d8b62d15ea7e4a
Willamar <wfs20@hotmail.com>
2018-12-10 22:05:40 -0200

    Atualização de novos comandos e inclusão de exemplos de chamada HTTTP e download de arquivos

--- Importing Data 2.R
+++ Importing Data 2.R
@@ -41,3 +41,74 @@ specific
 
 short <- dbGetQuery(con, "SELECT id, name from users where CHAR_LENGTH(name) < 5")
 short
+
+
+res <- dbSendQuery(con, "SELECT * FROM comments WHERE user_id > 4")
+dbFetch(res, n = 2)
+dbFetch(res, n = 2)
+dbClearResult(res)
+
+long_tweats <- dbGetQuery(con, "SELECT post, date from tweats where CHAR_LENGTH(post) > 40")
+long_tweats
+
+# DEsconecta da base de dados
+dbDisconnect(con)
+
+
+library(readr)
+url_csv <- "http://s3.amazonaws.com/assets.datacamp.com/production/course_1478/datasets/swimming_pools.csv"
+pools <- read_csv(url_csv)
+
+url_delim <- "http://s3.amazonaws.com/assets.datacamp.com/production/course_1478/datasets/potatoes.txt"
+potatoes <- read_tsv(url_delim)
+
+pools
+potatoes
+
+
+# https 
+url_csv <- "https://s3.amazonaws.com/assets.datacamp.com/production/course_1478/datasets/swimming_pools.csv"
+
+pools1 <- read.csv(url_csv)
+pools2 <- read_csv(url_csv)
+
+str(pools1)
+str(pools2)
+
+
+library(readxl)
+library(gdata)
+
+url_xls <- "http://s3.amazonaws.com/assets.datacamp.com/production/course_1478/datasets/latitude.xls"
+
+excel_gdata <- read.xls(url_xls)
+
+download.file(url_xls, "local_latitude.xls")
+
+excel_readxl <- read_excel("local_latitude.xls")
+
+
+url_rdata <- "https://s3.amazonaws.com/assets.datacamp.com/production/course_1478/datasets/wine.RData"
+download.file(url_rdata, "wine_local.RData")
+load("wine_local.RData")
+summary(wine)
+
+
+install.packages("httr")
+library(httr)
+
+url <- "http://www.example.com/"
+resp <- GET(url)
+
+resp
+
+raw_content <- content(resp, as = "raw")
+raw_content
+
+url <- "http://www.omdbapi.com/?apikey=ff21610b&t=Annie+Hall&y=&plot=short&r=json"
+resp <- GET(url)
+
+resp
+
+content(resp, as = "text")
+content(resp)
