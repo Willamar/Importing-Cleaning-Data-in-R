9133365e4f61194995be3df835ed85d92eff72ff
Willamar <wfs20@hotmail.com>
2019-01-03 21:37:34 -0200

    Atualização com novos comandos

--- Case Studies.R
+++ Case Studies.R
@@ -90,3 +90,142 @@ sales6 <- unite(sales5, venue_city_state,
 
 # View the head of sales6
 head(sales6)
+
+
+#Part 2
+# Load readxl
+library(readxl)
+
+# Import mbta.xlsx and skip first row: mbta
+mbta <- read_excel("DataSets/mbta.xlsx", skip=1)
+
+# View the structure of mbta
+str(mbta)
+
+# View the first 6 rows of mbta
+head(mbta)
+
+# View a summary of mbta
+summary(mbta)
+
+# Remove rows 1, 7, and 11 of mbta: mbta2
+mbta2 <- mbta[-c(1,7,11),]
+
+# Remove the first column of mbta2: mbta3
+mbta3 <- mbta2[,-1]
+
+# Load tidyr
+library(tidyr)
+install.packages("stringi")
+
+# Gather columns of mbta3: mbta4
+mbta4 <- gather(mbta3, month, thou_riders, -mode)
+
+# View the head of mbta4
+head(mbta4)
+
+# Coerce thou_riders to numeric
+mbta4$thou_riders <- as.numeric(mbta4$thou_riders)
+
+# Spread the contents of mbta4: mbta5
+mbta5 <- spread(mbta4, mode, thou_riders)
+
+# View the head of mbta5
+head(mbta5)
+
+
+# Split month column into month and year: mbta6
+mbta6 <- separate(mbta5, month, c("year", "month"), sep="-")
+
+# View the head of mbta6
+head(mbta6)
+
+# View a summary of mbta6
+summary(mbta6)
+
+# Generate a histogram of Boat column
+hist(mbta6$Boat)
+
+# Find the row number of the incorrect value: i
+i <- which(mbta6$Boat == 40)
+
+# Replace the incorrect value with 4
+mbta6[i,]$Boat <- 4
+
+# Generate a histogram of Boat column
+hist(mbta6$Boat)
+
+# Look at Boat and Trackless Trolley ridership over time (don't change)
+ggplot(mbta_boat, aes(x = month, y = thou_riders, col = mode)) +  geom_point() + 
+  scale_x_discrete(name = "Month", breaks = c(200701, 200801, 200901, 201001, 201101)) + 
+  scale_y_continuous(name = "Avg Weekday Ridership (thousands)")
+
+# Look at all T ridership over time (don't change)
+ggplot(mbta_all, aes(x = month, y = thou_riders, col = mode)) + geom_point() + 
+  scale_x_discrete(name = "Month", breaks = c(200701, 200801, 200901, 201001, 201101)) +  
+  scale_y_continuous(name = "Avg Weekday Ridership (thousands)")
+
+
+
+#PARTE 3
+# Load data.table
+library(data.table)
+
+# Import food.csv as a data frame: df_food
+food <- fread("DataSets/food.csv", data.table = FALSE)
+
+
+# View summary of food
+summary(food)
+
+# View head of food
+head(food)
+
+# View structure of food
+str(food)
+
+# Load dplyr
+library(dplyr)
+
+# View a glimpse of food
+glimpse(food)
+
+# View column names of food
+names(food)
+
+# Define vector of duplicate cols (don't change)
+duplicates <- c(4, 6, 11, 13, 15, 17, 18, 20, 22, 
+                24, 25, 28, 32, 34, 36, 38, 40, 
+                44, 46, 48, 51, 54, 65, 158)
+
+# Remove duplicates from food: food2
+food2 <- food[, -duplicates]
+
+# Define useless vector (don't change)
+useless <- c(1, 2, 3, 32:41)
+
+# Remove useless columns from food2: food3
+food3 <- food2[, -useless]
+
+library(stringr)
+# Create vector of column indices: nutrition
+nutrition <- str_detect(names(food3), "100g")
+
+# View a summary of nutrition columns
+summary(food3[, nutrition])
+
+
+# Find indices of sugar NA values: missing
+missing <- is.na(food3$sugars_100g)
+
+# Replace NA values with 0
+food3[missing,]$sugars_100g <- 0
+
+# Create first histogram
+hist(food3$sugars_100g, breaks = 100)
+
+# Create food4
+food4 <- food3[food3$sugars_100g > 0, ]
+
+# Create second histogram
+hist(food4$sugars_100g, breaks = 100)
